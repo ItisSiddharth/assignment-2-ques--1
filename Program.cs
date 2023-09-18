7bd17de5016f8ea28d7b7420b8c8e1f920dff1f7
sidharth <sidharthkumar@gmail.com>
2023-09-18 14:34:35 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e1 = new Employee(1, "sidharth", 201.61);
+            e1.displayData();
+        }
+    }
+    class Employee
+    {
+        public int id;
+        public string name;
+        public double salary;
+
+        public Employee(int id, string name, double salary)
+        {
+            this.id = id;
+            this.name = name;
+            this.salary = salary;
+        }
+        public void displayData()
+        {
+            Console.WriteLine("id :" + this.id);
+            Console.WriteLine("Name :" + this.name);
+            Console.WriteLine("Salary :" + this.salary);
+            Console.ReadKey();
+        }
+    }
+}
